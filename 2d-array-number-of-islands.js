f630298f769604a3f3c435ab4d69201b786fd63c
Panos Anastasiadis <anastapd@gmail.com>
2024-03-28 21:13:37 +0000

    islands 1st round

---
+++ 2d-array-number-of-islands.js
@@ -0,0 +1,91 @@
+function islands(arr) {
+    console.log(arr);
+    const visited = [];
+    let islands = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) {
+            if (arr[i][j] === 1 && !alreadyVisitedAt(visited, i, j)) {
+                islands += mapIsland(arr, [i, j], visited);
+            }
+        }
+    }
+    return islands;
+}
+
+function mapIsland(arr, item, visited) {
+    if (arr[item[0]][item[1]] !== 1) return 0;
+
+    const q = [];
+
+    q.push(item);
+    markAsVisited(visited, item);
+
+    const directions = resolveDirections(arr);
+
+    while (q.length > 0) {
+        const cur = q.shift();
+        for (let dir in directions) {
+            const child = directions[dir](...cur);
+            if (
+                child !== null &&
+                !alreadyVisitedAt(visited, ...child) &&
+                arr[child[0]][child[1]] === 1
+            ) {
+                q.push(child);
+                markAsVisited(visited, child);
+            }
+        }
+    }
+
+    return 1;
+}
+
+function alreadyVisitedAt(visited, r, c) {
+    return visited[r] !== undefined && visited[r][c] != undefined;
+}
+
+function resolveDirections(arr) {
+    return {
+        up: (i, j) => {
+            return i > 0 ? [i - 1, j] : null;
+        },
+        right: (i, j) => {
+            return j < arr[0].length - 1 ? [i, j + 1] : null;
+        },
+        down: (i, j) => {
+            return i < arr.length - 1 ? [i + 1, j] : null;
+        },
+        left: (i, j) => {
+            return j > 0 ? [i, j - 1] : null;
+        },
+    };
+}
+
+function markAsVisited(visited, [i, j]) {
+    if (visited[i] === undefined) {
+        visited[i] = [];
+    }
+
+    visited[i][j] = true;
+}
+
+const arr = [
+    [1, 1, 1, 1, 0],
+    [1, 1, 0, 1, 0],
+    [1, 1, 0, 0, 1],
+    [0, 0, 0, 1, 1],
+];
+
+console.log(islands(arr));
+
+console.log();
+
+const arr2 = [
+    [0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1],
+    [0, 1, 1, 1, 0],
+    [1, 0, 1, 0, 1],
+];
+
+console.log(islands(arr2));
